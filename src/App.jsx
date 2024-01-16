45c6f5b9dbe9e15ab689b30e8d47961b9557d7e8
FOKO-KENMOGNE-Wilfried <fokowilfried7@gmail.com>
2024-01-16 23:34:13 +0100

    The price bar is already ready

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,32 @@ const StyledSubDiv = styled.div`
   color: rgba(0, 0, 0, 0.4);
   z-index: 1;
 `
-
+const StyledInfo = styled.div`
+  position: absolute;
+  left: 0;
+`
+const StyledColorDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+`
+const StyledColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 30px;
+`
+const StyledNumberDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+`
+const StyledNumber = styled.div`
+  width: 35px;
+  height: 35px;
+  border-radius: 30px;
+  border: 1px solid black;
+  text-align: center;
+`
 function App() {
 
   return (
@@ -35,6 +60,29 @@ function App() {
       <StyledSubDiv>
         NIKE
       </StyledSubDiv>
+      <StyledInfo>
+        <p>NIKE Impact4</p>
+        <p>$250.90</p>
+        <div>
+          <p>Colors</p>
+          <StyledColorDiv>
+            <StyledColor style={{backgroundColor: "white"}}></StyledColor>
+            <StyledColor style={{backgroundColor: "red"}}></StyledColor>
+            <StyledColor style={{backgroundColor: "green"}}></StyledColor>
+            <StyledColor style={{backgroundColor: "black"}}></StyledColor>
+          </StyledColorDiv>
+        </div>
+        
+        <div>
+          <p>Size</p>
+          <StyledNumberDiv>
+            <StyledNumber>6</StyledNumber>
+            <StyledNumber>7</StyledNumber>
+            <StyledNumber>8</StyledNumber>
+            <StyledNumber>9</StyledNumber>
+          </StyledNumberDiv>
+        </div>
+      </StyledInfo>
 
     </StyledPage>
   )
